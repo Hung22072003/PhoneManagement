c49b372aa642f06377517f3490ad616ee55404f1
Laptop T&T <Laptop T&T@HUNGPC>
2023-03-31 13:51:23 +0700

    Add project files.

---
+++ PhoneManagement/DAL/DBHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneManagement.DAL
+{
+    internal class DBHelper
+    {
+        SqlConnection _cnn;
+        private static DBHelper _Instance;
+        public static DBHelper Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = new DBHelper("Data Source=HUNGPC\\HUNGSQLSERVER;Initial Catalog=QuanLyCuaHangDienThoai;Persist Security Info=True;User ID=hung2207;Password=123");
+                }
+                return _Instance;
+            }
+            private set { }
+        }
+
+        private DBHelper(string s)
+        {
+            _cnn = new SqlConnection(s);
+        }
+
+        public DataTable GetRecords(string query)
+        {
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(query, _cnn);
+            _cnn.Open();
+            da.Fill(dt);
+            _cnn.Close();
+            return dt;
+        }
+
+        public void ExcuteDB(string query)
+        {
+            SqlCommand cmd = new SqlCommand(query, _cnn);
+            _cnn.Open();
+            cmd.ExecuteNonQuery();
+            _cnn.Close();
+        }
+    }
+}
---
+++ PhoneManagement/DTO/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneManagement.DTO
+{
+    internal class Account
+    {
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+        public string DisplayName { get; set; }
+        public bool TypeAccount { get; set; }
+    }
+}
